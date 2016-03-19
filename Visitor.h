d2d798f4cbdd290119ba9c83ae8bfc8ae44b8fb8
ELE712 <zandber2@msu.edu>
2016-03-19 12:57:28 -0400

    Working Upload

--- Visitor.h
+++ Visitor.h
@@ -1,1 +1,30 @@
+/******************************************************
+ ** FILE: Visitor.h
+ **
+ ** ABSTRACT:
+ ** Abstract visitor class
+ **
+ **
+ ** AUTHOR: Ezekiel Zandbergen, Cynthia Obia
+ **
+ ** CREATION DATE: 3-14-15
+ **
+ *******************************************************/
+#ifndef VISITOR_H
+#define VISITOR_H
+
+class Group;
+class Department;
+class Employee;
+class Manager;
+
+class Visitor{
+public:
+    virtual void VisitGroup(Group*)=0;
+    virtual void VisitDepartment(Department*)=0;
+    virtual void VisitEmployee(Employee*)=0;
+    virtual void VisitManager(Manager*)=0;
+};
+
+#endif
 
