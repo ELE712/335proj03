a5582b7e07a9bc7626ae3e14acc2b26f3a8c9b3a
ELE712 <zandber2@msu.edu>
2016-03-19 12:56:07 -0400

    Working Upload

--- Group.h
+++ Group.h
@@ -1,1 +1,63 @@
+/******************************************************
+ ** FILE: Group.h
+ **
+ ** ABSTRACT:
+ ** Group class, group name and employees as members
+ **
+ **
+ ** AUTHOR: Ezekiel Zandbergen, Cynthia Obia
+ **
+ ** CREATION DATE: 3-14-15
+ **
+ *******************************************************/
+#ifndef GROUP_H
+#define GROUP_H
 
+#include <vector>
+#include <string>
+#include "Unit.h"
+
+
+class Group: public Unit{
+protected:
+    string m_name;
+    vector<Unit*> m_members;
+public:
+    Group(){
+        m_name = "";
+    }
+    Group(const string &name){
+        m_name = name;
+    }
+    virtual ~Group(){}
+    Group(const Group &group){
+        m_name = group.m_name;
+        m_members = group.m_members;
+    }
+    Group& operator=(Group group){
+        swap(m_name, group.m_name);
+        swap(m_members, group.m_members);
+        return *this;
+    }
+    //get and sets
+    string getName() const{
+        return m_name;
+    }
+    vector<Unit*> getMembers() const{
+        return m_members;
+    }
+    void setName(const string &name){
+        m_name = name;
+    }
+    void setMembers(vector<Unit*> members){
+        m_members = members;
+    }
+    void addGroupMember(Unit* mem){
+        m_members.push_back(mem);
+    }
+    virtual void Accept(Visitor* v){
+        v->VisitGroup(this);
+    }
+};
+
+#endif
