ee3759b31549d1c62d4726845cc87eaa917338dc
ELE712 <zandber2@msu.edu>
2016-03-19 12:57:04 -0400

    Working Upload

--- SumsalaryVisitor.h
+++ SumsalaryVisitor.h
@@ -1,1 +1,53 @@
+/******************************************************
+ ** FILE: SumsalaryVisitor.h
+ **
+ ** ABSTRACT:
+ ** Visitor class to add all salaries
+ **
+ **
+ ** AUTHOR: Ezekiel Zandbergen, Cynthia Obia
+ **
+ ** CREATION DATE: 3-14-15
+ **
+ *******************************************************/
+#ifndef SUMSALARYVISITOR_H
+#define SUMSALARYVISITOR_H
+#include "Visitor.h"
+#include "Department.h"
+#include "Group.h"
+#include "Manager.h"
+#include "Employee.h"
 
+class SumsalaryVisitor: public Visitor{
+protected:
+    unsigned int v_salary;
+public:
+    SumsalaryVisitor(){
+        v_salary = 0;
+    }
+    virtual ~SumsalaryVisitor(){}
+    virtual void VisitDepartment(Department* d){
+        for (int i=0; i<d->getSubDepartments().size(); i++){
+            d->getSubDepartments()[i]->Accept(this);
+        }
+    }
+    virtual void VisitGroup(Group* g){
+        for (int i=0; i<g->getMembers().size(); i++){
+            g->getMembers()[i]->Accept(this);
+        }
+    }
+    virtual void VisitManager(Manager* m){
+        v_salary += m->getSal();
+    }
+    virtual void VisitEmployee(Employee* e){
+        v_salary += e->getSal();
+    }
+    unsigned int getTotalSalary(){
+        return v_salary;
+    }
+    void restTotalSalary(){
+        v_salary = 0;
+    }
+};
+
+#endif
